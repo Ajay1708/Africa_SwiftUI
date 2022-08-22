941c97ae15cc8b11a9607a56d2a1d8edffafb247
Venkataparas <venkata@parastechnologies.com>
2022-08-22 20:57:52 +0530

    Initial Commit

---
+++ Africa_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Africa_SwiftUI
+//
+//  Created by Venkata Ajay Sai (Paras) on 22/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
